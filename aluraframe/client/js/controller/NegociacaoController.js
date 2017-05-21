2a56f9fd50d5104d7d1df10c1be4538d8e10b790
Luiz dos Reis <luizdosreis@gmail.com>
2017-05-20 21:38:39 -0300

    criando o datahelper

--- aluraframe/client/js/controller/NegociacaoController.js
+++ aluraframe/client/js/controller/NegociacaoController.js
@@ -12,18 +12,14 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-
         let negociacao = new Negociacao(
-            data,
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
+        let diaMesAno = DateHelper.dataParaTexto(negociacao.data);
+        console.log(diaMesAno);
     }
 
 }
