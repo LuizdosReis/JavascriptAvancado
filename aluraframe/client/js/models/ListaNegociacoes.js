929df67f70a351f7418e3b2c3caa992f040146ed
luizdosreis <luizdosreis@gmail.com>
2017-06-06 23:35:19 -0300

    formatacao do codigo e padrao proxy

--- aluraframe/client/js/models/ListaNegociacoes.js
+++ aluraframe/client/js/models/ListaNegociacoes.js
@@ -1,18 +1,15 @@
 class ListaNegociacoes{
 
-    constructor(updateView){
-        this._updateView = updateView;
+    constructor(){
         this._negociacoes = [];
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        this._updateView(this);
     }
 
     apaga(){
         this._negociacoes = [];
-        this._updateView(this);
     }
 
     get negociacoes(){
